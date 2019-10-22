407ecd1918db6bd65e405a5c7fada9e16e0a436d
jolion <jolion142580@163.com>
2019-10-22 17:51:54 +0800

    合并后冲突修改

--- out/artifacts/zcWechat_war_exploded/js/config.js
+++ out/artifacts/zcWechat_war_exploded/js/config.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-window.weChat={
- APPID : 'wx38df0e22682f9d12',
- APPSECRET : '2dbed8dbe0fd655a2cde81ed2a03c9c9',
- TOKEN : 'zcxzfwwx',
- // WeChatDNSURL: 'http://zcxzfwzx.chancheng.gov.cn/zcWechat/'
- WeChatDNSURL : 'http://2c48do4959.52http.net/'
- // WeChatDNSURL : 'http://localhost:8080/'
-=======
 window.weChat={
  APPID : 'wx8ee31ec6e6da3137',
  APPSECRET : '12bd7dfd65df749cb07db40988e0ae57',
@@ -14,5 +5,4 @@ window.weChat={
  // WeChatDNSURL: 'http://zcxzfwzx.chancheng.gov.cn/zcWechat/'
  // WeChatDNSURL : 'http://a6e09c8b.ngrok.io/'
  WeChatDNSURL : 'http://bin.ngrok.ibanzhuan.cn/'
->>>>>>> withoutWechatInterface
 }
